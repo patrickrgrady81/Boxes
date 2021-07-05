aef689abfa5e928fbfef4f47329c764867401561
Patrick Grady <patrickrgrady81@gmail.com>
2021-07-05 14:03:18 -0400

    setup complete

---
+++ canvas.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementsByClassName("canvas")[0];
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// ctx.fillStyle = "black";
+// ctx.fillRect(20, 40, 50, 50);
